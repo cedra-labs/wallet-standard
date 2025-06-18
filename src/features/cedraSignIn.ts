c6d39a1728750c71ebf7877c175c9d41bd45bb79
Inkvisto <qwertuasdfgh953@gmail.com>
2025-06-18 18:11:35 +0300

    fut: updated version

--- src/features/cedraSignIn.ts
+++ src/features/cedraSignIn.ts
@@ -12,7 +12,7 @@ import { AccountInfo } from '../AccountInfo'
  */
 
 /** Version of the feature. */
-export type CedraSignInVersion = '0.1.0'
+export type CedraSignInVersion = '1.0.0'
 /** Name of the feature. */
 export const CedraSignInNamespace = 'cedra:signIn'
 
@@ -30,20 +30,41 @@ export type CedraSignInMethod = (
 ) => Promise<UserResponse<CedraSignInOutput>>
 
 export type CedraSignInInput = {
+  /**
+Add comment
+More actions
+   * [`dnsauthority`](https://www.rfc-editor.org/rfc/rfc4501.html) that is requesting the signing.
+   *
+   * @example 'example.com'
+   */
+  domain: string
+  /**
+   * Randomized token to prevent signature replay attacks.
+   */
+  nonce: string
+  /**
+   * Blockchain address performing the signing.
+   *
+   * @example '0x10d7cf502f8571b5b6e402221cafb142547103da9c2847ffcf708f065a78b8d1'
+   */
   address?: string
   uri?: string
   version?: string
   statement?: string
-  nonce: string
   chainId?: string
   issuedAt?: string
   expirationTime?: string
   notBefore?: string
   requestId?: string
+  /**
+   * List of information or references to information the user wishes to have resolved as part of the authentication by the relying party.
+   *
+   * @example ['https://example.com/resource']
+   */
   resources?: string[]
 }
 
-export type CedraSignInRequiredFields = {
+export type CedraSignInBoundFields = {
   domain: string
   address: string
   uri: string
@@ -53,9 +74,15 @@ export type CedraSignInRequiredFields = {
 
 export type CedraSignInOutput = {
   account: AccountInfo
-  input: CedraSignInInput & CedraSignInRequiredFields
   plainText: string
   signingMessage: Uint8Array
+  /**Add commentMore actions
+   * Input fields to the `signIn` signing request to the wallet. The wallet will ensure that any bound fields not included in the `AptosSignInInput` are included in the output.
+   */
+  input: AptosSignInInput & AptosSignInBoundFields
+  /**
+   * Signature of the SIWA Signing Message constructed from the `input` fields.
+   */
   signature: Signature
   /**
    * The type of signing scheme used to sign the message.
